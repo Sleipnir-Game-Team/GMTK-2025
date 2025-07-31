46bfe908dfda56d6aeb42a8048e8fb01d2972276
FMABr <felipemuamorim@gmail.com>
2025-07-31 16:17:12 -0400

    dash

--- movement.gd
+++ movement.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export_group("Jump", "jump")
-
+#region
 ## Altura do pulo em termos da altura do personagem. Por exemplo: [br]
 ## • [code]2.0[/code]  - Pula duas vezes sua altura 
 @export var jump_height_factor: float = 2.0
@@ -16,20 +16,64 @@ extends CharacterBody2D
 ## Aplicado apenas quando o botão de pular é solto antes do pico. 
 @export var jump_cut_multiplier: float = 2.0
 
+## Tempo em segundos em que o comando de pulo é salvo caso o jogador não esteja no chão
+@export var jump_buffer_time: float = 0.2
+
+## Velocidade horizontal de saltos iniciados mid-dash.
+@export var jump_dash_boost: float = 2000.0
+
+## Tempo atual no buffer.[br]
+## Se está no chão e esse valor é acima de 0, pular.
+var jump_buffer: float = 0
+
+## Se o input de pulo atual foi iniciado num dash.
+var jump_boosted: bool = false
+#endregion
+
 @export_group("Movement", "movement")
+#region
+## Velocidade horizontal de movimento em pixels/seg
 @export var movement_walking_speed: float = 200.0
+
+## Multiplicador da velocidade horizontal enquanto em sprint
 @export_range(1, 10, .2) var movement_sprint_modifier: float = 2.0
+
+## Multiplicador da velocidade horizontal enquanto agachado
 @export_range(0.1, 1, .05) var movement_crouch_modifier: float = 0.8
+#endregion
+
+@export_subgroup("Dash", "dash")
+#region
+## Força do dash horizontal em pixels/seg
+@export var dash_speed: float = 2000.0
+
+## Duração do dash em segundos
+@export var dash_duration: float = 0.15
+
+## Desaceleração do dash em pixels/seg²
+@export var dash_deceleration: float = 12000
+var dash_time_left: float = 0.0
+var facing: int = 1
+#endregion
 
 @onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
 
 @onready var gravity_rise: float = _calculate_gravity_rise(jump_height_factor, jump_time_to_peak)
 @onready var gravity_fall: float = _calculate_gravity_fall(jump_height_factor, jump_time_to_descent)
 @onready var jump_speed: float = _calculate_jump_speed(gravity_rise, jump_time_to_peak)
+@onready var dash_acceleration: float = dash_speed / dash_duration
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
 
-var crouching: bool = false
+var crouching: bool = false:
+	set(_crouching):
+		crouching = _crouching
+		if crouching:
+			animation_player.play("player/crouch")
+		else:
+			animation_player.play_backwards("player/crouch")
 
 func _unhandled_key_input(event: InputEvent) -> void:
+	# Evitar dar o crouch quando já está em sprint
 	if event.is_action_pressed("player_crouch"):
 		crouching = true
 	elif event.is_action_released("player_crouch"):
@@ -43,27 +87,72 @@ func _physics_process(delta: float) -> void:
 	elif not Input.is_action_pressed("player_jump"):
 		# Se está subindo e soltou o botão de pulo, aumenta a gravidade para cair mais rápido
 		g *= jump_cut_multiplier
-	
 	velocity.y += g * delta
 	
-	velocity.x = 0.0
-	if Input.is_action_pressed("player_left"):
-		velocity.x = -movement_walking_speed
-	if Input.is_action_pressed("player_right"):
-		velocity.x = movement_walking_speed
+	# Captura do comando de pulo
+	if Input.is_action_just_pressed("player_jump"):
+		jump_buffer = jump_buffer_time
+		if _dashing():
+			# Pulo iniciado durante o dash
+			boosted_jump()
 	
-	if crouching:
-		velocity.x *= movement_crouch_modifier
-		 
-	elif Input.is_action_pressed("player_sprint"):
-		velocity.x *= movement_sprint_modifier
+	# Início de dash: sprint + crouch, precisa estar no chão
+	if not _dashing() \
+		and is_on_floor() \
+		and Input.is_action_pressed("player_sprint") \
+		and Input.is_action_just_pressed("player_crouch"):
+		dash_time_left = dash_duration
 	
+	if _dashing():
+		dash_time_left -= delta
+		# Velocidade constante do dash.
+		velocity.x = facing * dash_speed
 	
-	if is_on_floor() and Input.is_action_just_pressed("player_jump"):
-		velocity.y = jump_speed
+	if jump_boosted and not is_on_floor():
+		move_and_slide()
+		return
+	elif is_on_floor():
+		jump_boosted = false
+	
+	# Movimento horizontal (fora do dash)
+	if not _dashing():
+		var input_axis := Input.get_axis("player_left", "player_right")
+		if input_axis != 0:
+			facing = input_axis
+		
+		var target_speed := movement_walking_speed
+		# Agachado ignora completamente o sprint
+		if crouching:
+			target_speed *= movement_crouch_modifier
+		elif Input.is_action_pressed("player_sprint"):
+			target_speed *= movement_sprint_modifier
+		
+		velocity.x = input_axis * target_speed
+	
+	# Pulo normal
+	if _jump_buffered() \
+		and is_on_floor() \
+		and not _dashing():
+		jump()
+	else:
+		jump_buffer -= delta
 	
 	move_and_slide()
 
+func jump() -> void:
+	jump_buffer = 0
+	velocity.y = jump_speed
+
+func boosted_jump() -> void:
+	jump_boosted = true
+	velocity.x = facing * jump_dash_boost
+	jump()
+
+func _dashing() -> bool:
+	return dash_time_left > 0
+
+func _jump_buffered() -> bool:
+	return jump_buffer > 0
 
 func _falling() -> bool:
 	return velocity.y > 0
