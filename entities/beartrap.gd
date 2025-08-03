665fc03af958c4e3bc028051dbe3be49572343a9
Milton <milton_pinheiro@outlook.com>
2025-08-03 00:01:24 -0400

    some sprites and shit

--- entities/beartrap.gd
+++ entities/beartrap.gd
@@ -1,20 +1,35 @@
 extends Area2D
 
+var antisafadeza = false
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	$Duration.timeout.connect(_on_duration_timeout)
+	$Warmup.timeout.connect(_on_warmup_timeout)
+	$Cooldown.timeout.connect(_on_cooldown_timeout)
 
-func _on_body_entered(_body: Node2D) -> void:
-	$Warmup.start()
+func _process(delta):
+	$Sprite2D.visible = !$ActivatedArea/Collision.disabled
+	if !$Warmup.is_stopped():
+		$Incinerator.get_material().set_shader_parameter("unredness", ($Warmup.time_left/$Warmup.wait_time))
+	elif !$Duration.is_stopped() or !$Cooldown.is_stopped():
+		$Incinerator.get_material().set_shader_parameter("unredness", 0)
+	else:
+		$Incinerator.get_material().set_shader_parameter("unredness", 1)
 
+func _on_body_entered(body):
+	if !antisafadeza:
+		antisafadeza = true
+		$Collision.disabled = true
+		$Warmup.start()
 
 func _on_duration_timeout() -> void:
 	$ActivatedArea/Collision.disabled = true
-	$Collision.disabled = true
 	$Cooldown.start()
 
 
-func _on_cooldown_timeout() -> void:
+func _on_cooldown_timeout():
+	antisafadeza = false
 	$Collision.disabled = false
 
 
--- entities/laser.gd
+++ entities/laser.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
-func _on_cooldown_timeout() -> void:
+
+func _ready() -> void:
+	$Duration.timeout.connect(_on_duration_timeout)
+	$Cooldown.timeout.connect(_on_cooldown_timeout)
+	$Cooldown.start() 
+
+func _on_cooldown_timeout():
 	AudioManager.play_global("level.danger.electric")
 	$Collision.disabled = false
+	$Sprite.visible = true
 	$Duration.start()
 
 
 func _on_duration_timeout() -> void:
 	$Collision.disabled = true
+	$Sprite.visible = false
 	$Cooldown.start()
